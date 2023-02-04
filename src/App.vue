c3865468b66cad2dbc4ae9a1aa1fcd8c1263158c
Ivy <ivy.ember.raine@gmail.com>
2023-02-03 23:52:48 -0800

    CSS twiddling part 2

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,36 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 
-// timeGrid, 
 const calendarViewSelection = 'dayGridMonth';
 const calendarHeight = ref(window.innerHeight);
+const isUsingDayMaxEventRows = ref(true);
 
 const updateWeekNumbers = () => { return window.innerWidth < 350 ? false : true };
+const updateDayMaxEventRows = () => { return isUsingDayMaxEventRows.value ? -1 : Math.floor(window.innerHeight / 75) };
+
 const calendarOptions = ref({
   plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
   initialView: calendarViewSelection,
+  customButtons: {
+    less: {
+      text: 'less',
+      click: function () {
+        isUsingDayMaxEventRows.value = !isUsingDayMaxEventRows.value;
+        calendarOptions.value = {
+          ...calendarOptions.value,
+          dayMaxEventRows: updateDayMaxEventRows()
+        };
+      }
+    }
+  },
   headerToolbar: {
-    left: 'prev,next today',
+    left: 'prev,next today,less',
     center: 'title',
     right: 'dayGridMonth timeGridDay,listDay'
   },
   nowIndicator: true,
   height: '100vh',
-  dayMaxEventRows: 8,
+  dayMaxEventRows: updateDayMaxEventRows(),
   navLinks: true,
   weekNumbers: updateWeekNumbers(),
   events: [
@@ -39,11 +53,17 @@ const calendarOptions = ref({
 
 const updateCalendarHeight = () => {
   calendarHeight.value = window.innerHeight;
-  calendarOptions.value = { ...calendarOptions.value, weekNumbers: window.innerWidth < 350 ? false : true };
+  calendarOptions.value = {
+    ...calendarOptions.value,
+    weekNumbers: window.innerWidth < 350 ? false : true,
+    dayMaxEventRows: updateDayMaxEventRows()
+  };
 };
 
 onMounted(() => {
   window.addEventListener("resize", updateCalendarHeight);
+  console.log(isUsingDayMaxEventRows.value);
+
 })
 onUnmounted(() => window.removeEventListener('resize', updateWeekNumbers));
 
