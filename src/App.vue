9cb3cb24c6684ab2568d38feeb52c51ff42a58c0
Ivy <ivy.ember.raine@gmail.com>
2023-02-05 13:24:51 -0800

    Add first event source!

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted, onUpdated } from 'vue'
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 
+// The Event object is based on https://fullcalendar.io/docs/event-object, as well as 
+interface Event {
+  title: string | null;
+  start: Date | null;
+  end: Date | null;
+  url: string;
+  display?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+  textColor?: string;
+  classNames?: string[];
+  extendedProps?: Object;
+}
+
+interface EventSource {
+  events: Event[];
+}
+
+const eventSources = ref([]);
 const calendarViewSelection = 'dayGridMonth';
 const calendarHeight = ref(window.innerHeight);
 const pageWidth = ref(window.innerWidth);
@@ -38,18 +57,8 @@ const calendarOptions = ref({
   dayMaxEventRows: updateDayMaxEventRows(),
   navLinks: true,
   weekNumbers: updateWeekNumbers(),
-  events: [
-    { title: 'nrspr', start: new Date() },
-    { title: 'nrspr', start: new Date() },
-    { title: 'nrspr', start: new Date() },
-    { title: 'nrspr', start: new Date() },
-    { title: 'nrspr', start: new Date() },
-    { title: 'nrspr', start: new Date() },
-    { title: 'nrspr', start: new Date() },
-    { title: 'nrspr', start: new Date() },
-    { title: 'nrspr', start: new Date() },
-    { title: 'nrspr', start: new Date() }
-  ],
+  eventSources: eventSources.value,
+  progressiveEventRendering: true, // More re-renders; not batched. Needs further testing.
   stickyHeaderDates: true,
 });
 
@@ -64,7 +73,81 @@ const updateCalendarHeight = () => {
 };
 
 onMounted(() => window.addEventListener("resize", updateCalendarHeight));
+// onUpdated(() => {});
 onUnmounted(() => window.removeEventListener('resize', updateWeekNumbers));
+
+// Converts a schema.org event to a FullCalendar event.
+function convertSchemaDotOrgEventToFullCalendarEvent(item) {
+  // If we have a `geo` object, format it to geoJSON.
+  var geoJSON = (item.location.geo) ? {
+    type: "Point",
+    coordinates: [
+      item.location.geo.longitude,
+      item.location.geo.latitude
+    ]
+  } : null; // Otherwise, set it to null.
+  return {
+    title: item.name,
+    start: item.startDate,
+    end: item.endDate,
+    url: item.url,
+    extendedProps: {
+      description: item.description || null,
+      image: item.image,
+      location: {
+        geoJSON: geoJSON,
+        eventVenue: {
+          name: item.location.name,
+          address: {
+            streetAddress: item.location.streetAddress,
+            addressLocality: item.location.addressLocality,
+            addressRegion: item.location.addressRegion,
+            postalCode: item.location.postalCode,
+            addressCountry: item.location.addressCountry
+          },
+          geo: item.location?.geo
+        }
+      }
+    }
+  };
+};
+
+// Updates calendarOptions' eventSources and triggers a re-render of the calendar.
+function addEventSource(eventSource: EventSource) {
+  // Issue: might take a long time to actually update the calendar if the list of, for example, Eventbrite events/sources is large.
+  calendarOptions.value = {
+    ...calendarOptions.value,
+    eventSources: eventSources.value.concat(eventSource)
+  };
+}
+
+async function loadEvents() {
+  const toCorsProxy = (url: string) => 'https://corsproxy.io/?' + encodeURIComponent(url);
+  const domParser = new DOMParser();
+  const eventSourcesFromFile = await (await fetch('event_sources.json')).json();
+
+  // Eventbrite
+  let eventBriteSources = await Promise.all(
+    eventSourcesFromFile.eventbrite.map(async (source: Event) =>
+      await fetch(toCorsProxy(source.url))
+        .then(res => res.text())
+        .then(html => {
+          const events = JSON.parse(
+            domParser.parseFromString(html, 'text/html')
+              .querySelectorAll('script[type="application/ld+json"]')[1].innerHTML
+          ).map(convertSchemaDotOrgEventToFullCalendarEvent);
+          return {
+            events
+          } as EventSource;
+        })
+    )
+  );
+  addEventSource(eventBriteSources);
+
+
+}
+
+loadEvents()
 </script>
 
 <template>
@@ -74,33 +157,30 @@ onUnmounted(() => window.removeEventListener('resize', updateWeekNumbers));
     <img class="cat" src="cat.gif" alt="cat moving" v-bind:width=pageWidth />
     <FullCalendar :options='calendarOptions' />
 
-    <div style="display: flex; align-items: center;">
-      <img class="gifs" src="shark.gif" alt="shark dancing" :width='Math.min(pageWidth / 3, 600)' />
-      <img class="gifs" src="balloons.gif" alt="balloons and words that say 'INTERNET PARTY'"
-        :width='Math.min(pageWidth / 3, 600)' />
-      <img class="gifs" src="bmo.gif" alt="BMO dancing" :width='Math.min(pageWidth / 3, 600)' />
-    </div>
-
-    <div class="desc">
-      <p><strong>This board was made because I felt events in the Bay were hard to find! The events here are scraped
-          from various venue event listings.</strong> Before
-        making plans, consider checking with venue staff or event organizers directly. Come out and dance!
-      </p>
-      <p>In New York? Check out our sister site at <a href="https://anarchism.nyc/">anarchism.nyc</a>- to who I owe
-        the
-        inspiration.</p>
-      <p><strong>Want your event listed here?</strong> You must be publishing a machine-readable feed of event data
-        formatted in <a href="https://fullcalendar.io/docs/event-source">a compatible Event Source format</a>. (This
-        can
-        be as simple as a <a href="https://support.google.com/calendar/answer/37083">public Google Calendar</a>.) Once
-        published, request inclusion of your event feed by <a
-          href="https://github.com/ivyraine/bay.lgbt/issues">submitting
-          your event feed address to us via a new GitHub issue</a>. You may also provide feedback, fixes, or
-        improvements there!</p>
+    <div style="display: flex; align-items: center; flex-direction: row;">
+      <!-- <img class="gifs" src="shark.gif" alt="shark dancing" :width='Math.min(pageWidth / 3, 600)' /> -->
+      <!-- <img class="gifs" src="balloons.gif" alt="balloons and words that say 'INTERNET PARTY'" -->
+      <!-- :width='Math.min(pageWidth / 3, 600)' /> -->
+      <div class="desc">
+        <p><strong>The events here are scraped
+            from various venue event listings.</strong> Before
+          making plans, consider checking with venue staff or event organizers directly. Come out and dance!
+        </p>
+        <p>In New York? Check out our sister site at <a href="https://anarchism.nyc/">anarchism.nyc</a>- to who I owe
+          the
+          inspiration.</p>
+        <p><strong>Want your event listed here?</strong> You must be publishing a machine-readable feed of event data
+          formatted in <a href="https://fullcalendar.io/docs/event-source">a compatible Event Source format</a>. (This
+          can
+          be as simple as a <a href="https://support.google.com/calendar/answer/37083">public Google Calendar</a>.) Once
+          published, request inclusion of your event feed by <a href="https://github.com/ivyraine/bay.lgbt/issues">submitting
+            your event feed address to us via a new GitHub issue</a>. You may also provide feedback, fixes, or
+          improvements there!</p>
+      </div>
+      <img class="gifs" src="bmo.gif" alt="BMO dancing" :width='Math.min(pageWidth / 3, 400)' />
     </div>
   </div>
 </template>
 
 <style scoped>
-
 </style>
