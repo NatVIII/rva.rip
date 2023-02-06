d9099d82e3139412014bc212917a17f87a48c48a
Ivy <ivy.ember.raine@gmail.com>
2023-02-06 00:20:33 -0800

    Remove HTTPS dev option

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    basicSsl() // Disable in production. Used for localhost HTTPS testing.
+    // basicSsl() // Disable in production. Used for localhost HTTPS testing.
   ],
 })
