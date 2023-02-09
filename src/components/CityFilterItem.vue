d714db07ba6bf60b0061f17fa2eb9b0d43d8c4ea
Ivy <ivy.ember.raine@gmail.com>
2023-02-08 20:10:43 -0800

    Add missing files

---
+++ src/components/CityFilterItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed, onUpdated } from 'vue';
+const props = defineProps({ label: String, modelValue: Boolean });
+const emit = defineEmits(['update:modelValue', 'onInput']);
+const value = computed({
+	get() {
+		return props.modelValue
+	},
+	set(checked) {
+		emit('update:modelValue', checked)
+	}
+})
+</script>
+
+<template>
+	<label class="city-header">
+		<input type="checkbox" v-model="value" @click.passive="$emit('onInput', $event)"> {{ label }}
+	</label>
+</template>
---
+++ src/components/CountyFilterItem.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps({
+	label: String,
+});
+</script>
+<template>
+	<div class="county-header">
+		<span>
+			<button class="county-input" @click.passive="$emit('onYes')">Y</button>
+			<button class="county-input" @click.passive="$emit('onNo')">N</button>
+			{{ label }}
+		</span>
+	</div>
+</template>
