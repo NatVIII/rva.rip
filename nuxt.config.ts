a6de76e5fc8613292d2308a86ea0d7b395484553
Ivy <ivy.ember.raine@gmail.com>
2023-02-16 18:51:14 -0800

    Add rate limiting and HTTP method restricter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,34 @@ export default defineNuxtConfig({
 		// This ignores errors on build too.
 		typeCheck: false
 	},
-	css: ['vue-final-modal/style.css'],
+	modules: [
+		'nuxt-security'
+	],
+	// See https://nuxt-security.vercel.app/getting-started/quick-start for info on security.
+	security: {
+		rateLimiter: {
+			value: {
+				tokensPerInterval: 18,
+				interval: "hour",
+				fireImmediately: false
+			},
+			route: '',
+			throwError: false, // optional
+		},
+		requestSizeLimiter: {
+			value: {
+				maxRequestSizeInBytes: 8000, // Most browsers have a request limit of 8KB, but this is to be safe.
+				maxUploadFileRequestInBytes: 1000000,
+			},
+			route: '',
+			throwError: false // optional,
+		},
+		allowedMethodsRestricter: {
+			value: ['GET'],
+			route: '',
+			throwError: false, // optional
+		}
+	},
 	plugins: [{ src: '~/plugins/vercel.ts', mode: 'client' }],
+	css: ['vue-final-modal/style.css'],
 })
