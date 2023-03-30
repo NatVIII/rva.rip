5723a43275507a71ad8a37c4361ec1b23fbb5508
Ivy <ivy.ember.raine@gmail.com>
2023-03-29 20:11:36 -0700

    Prevent OpenAI from hallucinating event start time

--- server/api/events/instagram.ts
+++ server/api/events/instagram.ts
@@ -258,6 +258,9 @@ async function fetchInstagramEvents() {
 						`"title": string | null,\n` +
 						`"startDay": number | null,\n` +
 						`"endDay": number | null,\n` +
+						// Putting `isPastEvent` and `startHourMilitaryTime` knocks some sense into the model into not imagining start times. 
+						`"isPastEvent": boolean,\n` +
+						`"hasStartHourInPost": boolean,\n` +
 						`"startHourMilitaryTime": number | null,\n` +
 						`"endHourMilitaryTime": number | null,\n` +
 						`"startMinute": number | null,\n` +
@@ -269,36 +272,36 @@ async function fetchInstagramEvents() {
 						` }\n` +
 						"```\n" +
 						"Here's some important information regarding the post information:\n" +
-						"-Information regarding time provided by the caption is guaranteed to be correct. However, the caption might be lacking information regarding time and title.\n" +
-						"-The OCR result is provided by an OCR AI & thus may contain errors. Use it as a supplement for the information provided in the caption! This is especially useful when the caption is lacking information. The OCR Result also may contain time or title information that's not provided by the caption!\n" +
-						"-Sometimes a person or artist's username and their actual name can be found in the caption and OCR result; the username can be indicated by it being all lowercase and containing `.`s or `_`s. Their actual names would have very similar letters to the username, and might be provided by the OCR result. If the actual name is found, prefer using it for the JSON title, otherwise use the username.\n" +
+						"- Information regarding time provided by the caption is guaranteed to be correct. However, the caption might be lacking information regarding time and title.\n" +
+						"- The OCR result is provided by an OCR AI & thus may contain errors. Use it as a supplement for the information provided in the caption! This is especially useful when the caption is lacking information. The OCR Result also may contain time or title information that's not provided by the caption!\n" +
+						"- Sometimes a person or artist's username and their actual name can be found in the caption and OCR result; the username can be indicated by it being all lowercase and containing `.`s or `_`s. Their actual names would have very similar letters to the username, and might be provided by the OCR result. If the actual name is found, prefer using it for the JSON title, otherwise use the username.\n" +
 						"Here are some additional rules you should follow:\n" +
-						`-The post was posted on ${new Date(event.timestamp).toDateString()}. The time it was posted itself is not an event start time. However, if the post is about an event, the post time can be used to extrapolate event times relative to today, time-relative wording is used; for example, if the event starts 'tomorrow', you can determine that the event begins 1 day after today's date.\n` +
-						"-If no start day is explicitly provided by the caption or OCR result, and it cannot be inferred using relative times, assign `startDay` to null.\n" +
-						"-If no end day is explicitly provided by the caption or OCR result, assign `endDay` to null.\n" +
+						`- The post was posted on ${new Date(event.timestamp).toDateString()}. The time it was posted itself is not an event start time. However, if the post is about an event, the post time can be used to extrapolate event times relative to today, time-relative wording is used; for example, if the event starts 'tomorrow', you can determine that the event begins 1 day after today's date.\n` +
+						"- If no start day is explicitly provided by the caption or OCR result, and it cannot be inferred using relative times, assign `startDay` to null.\n" +
+						"- If no end day is explicitly provided by the caption or OCR result, assign `endDay` to null.\n" +
 						// "-If only one time is provided in the caption or OCR result, assume it's the start time.\n" +
-						"-If no start hour is explicitly provided by the caption or OCR result, such as ('2 pm' or the '9' in '9-12'), you must assign `startHourMilitaryTime` to null, even if the post is about an event.\n" +
-						"-If no end hour is explicitly provided by the caption or OCR result, assign `endHourMilitaryTime` to null.\n" +
+						"- If no start hour is explicitly provided by the caption or OCR result, such as ('2 pm' or the '9' in '9-12'), you must assign `startHourMilitaryTime` to null, even if the post is about an event.\n" +
+						"- If no end hour is explicitly provided by the caption or OCR result, assign `endHourMilitaryTime` to null.\n" +
 						// "-If the end hour is less than the start hour (for example, 9 to 2), assume the event ends on the day after the starting day.\n" +
-						"-If no start minute is explicitly provided by the caption or OCR result, assign `startMinute` to null.\n" +
-						"-If no end minute is explicitly provided by the caption or OCR result, assign `endMinute` to null.\n" +
-						"-If the end time states 'late' or similar, assume it ends around 2 AM on the next day from `startDay`.\n" +
-						"-If the end time states 'morning' or similar, assume it ends around 6 AM on the next day from `startDay`.\n" +
-						"-If no end month is explicitly provided by the caption or OCR result, assign it to the same month as startMonth.\n" +
-						`-If no start or end year are explicity provided, assume they are both the current year of ${new Date().getFullYear()}.\n` +
-						"-If the start hour is PM and the end hour is AM, assume the event ends on the next day from the starting day.\n" +
+						"- If no start minute is explicitly provided by the caption or OCR result, assign `startMinute` to null.\n" +
+						"- If no end minute is explicitly provided by the caption or OCR result, assign `endMinute` to null.\n" +
+						"- If the end time states 'late' or similar, assume it ends around 2 AM on the next day from `startDay`.\n" +
+						"- If the end time states 'morning' or similar, assume it ends around 6 AM on the next day from `startDay`.\n" +
+						"- If no end month is explicitly provided by the caption or OCR result, assign it to the same month as startMonth.\n" +
+						`- If no start or end year are explicity provided, assume they are both the current year of ${new Date().getFullYear()}.\n` +
+						"- If the start hour is PM and the end hour is AM, assume the event ends on the next day from the starting day.\n" +
 						// Do this to prevent it from making adjustments to the time.
-						"-Don't make any timezone-related adjustments to the times; assume it is UTC already.\n" +
-						"-Don't add any extra capitalization or spacing to the title that wasn't included in the post's information.\n" +
-						"-If the title of the event is longer than 210 characters, shorten it to include just the most important parts.\n" +
+						"- Don't make any timezone-related adjustments to the times; assume it is UTC already.\n" +
+						"- Don't add any extra capitalization or spacing to the title that wasn't included in the post's information.\n" +
+						"- If the title of the event is longer than 210 characters, shorten it to include just the most important parts.\n" +
 						// The following is to avoid organizer meetings.
-						"-If post contains multiple different events, only output the result for the earliest event.\n" +
-						"-If the event is explicity 'private', or a 'meeting', then set the start hour to null.\n" +
-						`${tags_string.toLowerCase().includes('music') ? "-Add \`&\` in between multiple music artist names, if any exist.\n" : ""}` +
-						`${tags_string.toLowerCase().includes('music') ? "-Include featured music artists in the title as well.\n" : ""}` +
-						`${(caption.includes('🎱') || event.ocrResult.includes('🎱')) ? "-Consider 🎱 to be read as `8` if given as a time. \n" : ""}` +
-						"-Do not include any other text in your response besides the raw JSON." + "\n" +
-						'-Make sure JSON format is valid (i.e. contains commas after each field, except for the last one).' + "\n" +
+						"- If post contains multiple different events, only output the result for the earliest event.\n" +
+						"- If the event is explicity 'private', or a 'meeting', then set the start hour to null.\n" +
+						`${tags_string.toLowerCase().includes('music') ? "- Add \`&\` in between multiple music artist names, if any exist.\n" : ""}` +
+						`${tags_string.toLowerCase().includes('music') ? "- Include featured music artists in the title as well.\n" : ""}` +
+						`${(caption.includes('🎱') || event.ocrResult.includes('🎱')) ? "- Consider 🎱 to be read as `8` if given as a time. \n" : ""}` +
+						"- Do not include any other text in your response besides the raw JSON." + "\n" +
+						'- Make sure JSON format is valid (i.e. contains commas after each field, except for the last one).' + "\n" +
 						"\n" +
 						"A:";
 
