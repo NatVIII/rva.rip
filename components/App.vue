b68e7bb1c5ea4ccbf3dc69e7e660340c40931bdd
Ivy <ivy.ember.raine@gmail.com>
2023-04-11 16:54:49 -0700

    Update description to remove BMO, add other references.

--- components/App.vue
+++ components/App.vue
@@ -451,23 +451,39 @@ function updateCityIsEnabledSetting(newIsEnabled: boolean, cityId: string) {
 
 <template>
   <ModalsContainer />
-    <div class="calendar-container">
-      <div style="display: flex; flex-direction:column; position:relative;">
-        <div class="title">bay.lgbt</div>
-        <div style="display:flex; flex-direction: column; align-items: center;">
-          <div class="blurb">A communal board for LGBT events all around SF bay! Come out and play ;3</div>
-        </div>
+  <div class="calendar-container">
+    <div style="display: flex; flex-direction:column; position:relative;">
+      <div class="title">bay.lgbt</div>
+      <div style="display:flex; flex-direction: column; align-items: center;">
+        <div class="blurb">A communal board for LGBT events all around SF bay! Come out and play ;3</div>
       </div>
-      <FullCalendar :options='calendarOptions'/>
-      <div style="display: flex; align-items: center; flex-direction: row;">
-        <div class="desc">
-          <p>bay.lgbt was built with the personal hope that no LGBTQ+ person should be without community. The site will always be free, without frills, and remain a public utility. The events here are drawn from various <a href="https://github.com/ivyraine/bay.lgbt/blob/main/public/event_sources.json">organizer listings</a> that contributors (thank you!) have provided. The listings are in a constant state of community-based vetting; don't hesitate to provide feedback <a href="https://forms.gle/DMt1xKyMKbHCsZMv5">here</a>!</p>
-          <p>Before making plans, consider checking with venue staff or event organizers directly. This site is not affiliated with any events listed.</p>
-          <p>In New York? Check out our sister site at <a href="https://anarchism.nyc/">anarchism.nyc</a>- to who I owe the inspiration.</p>
-          <p>Want your event listed here? You must be publishing a machine-readable feed of event data formatted in <a href="https://fullcalendar.io/docs/event-source">a compatible Event Source format</a>. (This can be as simple as a <a href="https://support.google.com/calendar/answer/37083">public Google Calendar</a>.) Once published, request inclusion of your event feed by <a href="https://github.com/ivyraine/bay.lgbt/issues">submitting your event feed address to us via a new GitHub issue</a>. You may also provide feedback, fixes, or improvements there! Thanks to recent advances in AI, you may also share your events as Instagram posts, but it comes at the expense of accuracy and Ivy's budget (nonexistent). Donations are greatly appreciated and can be made <a href='https://ko-fi.com/ivyraine'>here</a>!</p>
-            <a href="https://raw.githubusercontent.com/ivyraine/bay.lgbt/main/public/event_sources.json">event sources</a> | <a href="https://github.com/ivyraine/bay.lgbt/">source code</a>
+    </div>
+    <FullCalendar :options='calendarOptions' />
+    <div style="display: flex; align-items: center; flex-direction: row;">
+      <div class="desc">
+        <p>bay.lgbt was built with the personal hope that no LGBTQ+ person should be without community. The site will
+          always be free, without frills, and remain a public utility. The events here are drawn from various <a
+            href="https://github.com/ivyraine/bay.lgbt/blob/main/public/event_sources.json">organizer listings</a> that
+          contributors (thank you!) have provided. The listings are in a constant state of community-based vetting; don't
+          hesitate to provide feedback <a href="https://forms.gle/DMt1xKyMKbHCsZMv5">here</a>!</p>
+        <p>Before making plans, consider checking with venue staff or event organizers directly. This site is not
+          affiliated with any events listed.</p>
+        <p>In New York? Check out our sister site at <a href="https://anarchism.nyc/">anarchism.nyc</a>- this site
+          wouldn't exist without it.</p>
+        <p>Looking for BDSM events? Check out <a href="https://erobay.com/">erobay.com</a>.</p>
+        <p>Looking for more electronic music? Check out <a
+            href="https://19hz.info/eventlisting_BayArea.php">19hz.info</a>.</p>
+        <p>Want your event listed here? You must be publishing a machine-readable feed of event data formatted in <a
+            href="https://fullcalendar.io/docs/event-source">a compatible Event Source format</a>. (This can be as simple
+          as a <a href="https://support.google.com/calendar/answer/37083">public Google Calendar</a>.) Once published,
+          request inclusion of your event feed by <a href="https://github.com/ivyraine/bay.lgbt/issues">submitting your
+            event feed address to us via a new GitHub issue</a>. You may also provide feedback, fixes, or improvements
+          there! Thanks to recent advances in AI, you may also share your events as Instagram posts, but it comes at the
+          expense of accuracy and Ivy's budget (nonexistent). Donations are greatly appreciated and can be made <a
+            href='https://ko-fi.com/ivyraine'>here</a>!</p>
+        <a href="https://raw.githubusercontent.com/ivyraine/bay.lgbt/main/public/event_sources.json">event sources</a> |
+        <a href="https://github.com/ivyraine/bay.lgbt/">source code</a>
       </div>
-          <img class="gifs" src="/bmo.gif" alt="BMO dancing" :width='Math.min(pageWidth / 5, 400)' />
     </div>
   </div>
 </template>
