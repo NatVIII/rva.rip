617895e60fc32e90ddc0d4d7be8501e959ded21f
Ivy <ivy.ember.raine@gmail.com>
2023-06-01 18:57:02 -0700

    Sensationalize Instagram event titles

--- server/api/events/instagram.ts
+++ server/api/events/instagram.ts
@@ -288,6 +288,7 @@ async function fetchInstagramEvents() {
 						"- The OCR result is provided by an OCR AI & thus may contain errors. Use it as a supplement for the information provided in the caption! This is especially useful when the caption is lacking information. The OCR Result also may contain time or title information that's not provided by the caption!\n" +
 						"- Sometimes a person or artist's username and their actual name can be found in the caption and OCR result; the username can be indicated by it being all lowercase and containing `.`s or `_`s. Their actual names would have very similar letters to the username, and might be provided by the OCR result. If the actual name is found, prefer using it for the JSON title, otherwise use the username.\n" +
 						"Here are some additional rules you should follow:\n" +
+						"- Use the most sensational title you can extract!\n" +
 						`- The post was posted on ${new Date(event.timestamp).toDateString()}. The time it was posted itself is not an event start time. However, if the post is about an event, the post time can be used to extrapolate event times relative to today, time-relative wording is used; for example, if the event starts 'tomorrow', you can determine that the event begins 1 day after today's date.\n` +
 						"- If no start day is explicitly provided by the caption or OCR result, and it cannot be inferred using relative times, assign `startDay` to null.\n" +
 						"- If no end day is explicitly provided by the caption or OCR result, assign `endDay` to null.\n" +
