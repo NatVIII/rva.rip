90b160f54b247178bc2ec2e5f22e7e2dbbcf0112
NatVIII <natviii@users.noreply.github.com>
2023-09-22 11:38:54 -0400

    first version of events filter

--- components/FilterModal.vue
+++ components/FilterModal.vue
@@ -1,21 +1,37 @@
 <script setup lang="ts">
 import { VueFinalModal } from 'vue-final-modal'
 const props = defineProps<{
-	title?: string
-	allCallback: (newIsEnabled: boolean) => void
-	cityCallback: (newIsEnabled: boolean, city: string) => void
-	countyCallback: (newIsEnabled: boolean, county: string) => void
+  title?: string
+  events: any[] // Add this prop to pass the event data
 }>()
 const emit = defineEmits<{
-	(e: 'confirm'): void
+  (e: 'confirm'): void
 }>()
+
+const toggleTag = (event) => {
+  // You can perform any necessary logic here, e.g., update the state of the tags
+  // and call the appropriate callback functions based on the checked state.
+  if (event.checked) {
+    // Tag is checked, you can implement your logic here
+  } else {
+    // Tag is unchecked, you can implement your logic here
+  }
+}
 </script>
 
 <template>
   <VueFinalModal class="popper-box-wrapper" content-class="popper-box-inner" overlay-transition="vfm-fade" content-transition="vfm-fade">
     <!-- Display Event Details -->
     <div class="event-details">
-      <span class="event-headers">Hello World!</span>
+      <span class="event-headers">Filter by Tags:</span>
+      <ul>
+        <li v-for="event in props.events" :key="event.id">
+          <label>
+            <input type="checkbox" v-model="event.checked" @change="toggleTag(event)">
+            {{ event.tags }}
+          </label>
+        </li>
+      </ul>
     </div>
 
     <!-- Add a "Done" button -->
