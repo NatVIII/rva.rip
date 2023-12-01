478c9160142b7e337c7c5fc94400ee4a5aecfeb4
NatVIII <natviii@users.noreply.github.com>
2023-12-01 14:35:12 -0500

    Trying to get more of a pulse on onMounted

--- components/App.vue
+++ components/App.vue
@@ -320,7 +320,7 @@ onMounted(() => {
   window.addEventListener("resize", updateCalendarHeight);
   moveListViewScrollbarToTodayAndColor();
   calendarApi = this.$refs.fullCalendar.getApi();
-  console.log(calendarApi);  // add this line
+  console.log("CalendarAPI: "+calendarApi);  // add this line
 });
 onUpdated(() => {
 });
