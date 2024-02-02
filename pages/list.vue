ee966f845539a2c88b072edde07a95a599ed8b6e
Danny <daniel.kim@deptagency.com>
2024-02-01 22:13:32 -0500

    Add RVA Queer Pickleball and format

--- pages/list.vue
+++ pages/list.vue
@@ -2,91 +2,53 @@
     <h1>Richmond Community Events & Organizations</h1>
     <h2>Events Aggregator</h2>
     <ul>
-        <li>
-            <a href="https://linktr.ee/commonalityrva" target="_blank">Commonality RVA</a>
-        </li>
+        <li><a href="https://linktr.ee/commonalityrva" target="_blank">Commonality RVA</a></li>
     </ul>
     <h2>Labor Rights</h2>
     <ul>
-        <li>
-            <a href="https://linktr.ee/workers.roar" target="_blank">Richmond Organizing and Agitating Restaurants</a>
-        </li>
+        <li><a href="https://linktr.ee/workers.roar" target="_blank">Richmond Organizing and Agitating Restaurants</a></li>
     </ul>
     <h2>Mutual Aid</h2>
     <ul>
-        <li>
-            <a href="https://rvabailfund.org" target="_blank">Richmond Community Bail Fund</a>
-        </li>
-        <li>
-            <a href="https://linktr.ee/rivercityharmredux" target="_blank">River City Harm Redux</a>
-        </li>
-        <li>
-            <a href="https://linktr.ee/richmondFNB" target="_blank">Richmond Food Not Bombs</a>
-        </li>
-        <li>
-            <a href="https://www.rvacommunityfridges.com" target="_blank">RVA Community Fridges</a>
-        </li>
-        <li>
-            <a href="https://www.madrva.org" target="_blank">Mutual Aid Distribution Richmond</a>
-        </li>
+        <li><a href="https://rvabailfund.org" target="_blank">Richmond Community Bail Fund</a></li>
+        <li><a href="https://linktr.ee/rivercityharmredux" target="_blank">River City Harm Redux</a></li>
+        <li><a href="https://linktr.ee/richmondFNB" target="_blank">Richmond Food Not Bombs</a></li>
+        <li><a href="https://www.rvacommunityfridges.com" target="_blank">RVA Community Fridges</a></li>
+        <li><a href="https://www.madrva.org" target="_blank">Mutual Aid Distribution Richmond</a></li>
     </ul>
     <h2>Climate Justice</h2>
     <ul>
-        <li>
-            <a href="https://linktr.ee/SunriseMvmtRVA" target="_blank">Sunrise Movement RVA</a>
-        </li>
-        <li>
-            <a href="https://www.instagram.com/richmond_wds" target="_blank">Richmond Weelaunee Defense Society</a>
-        </li>
+        <li><a href="https://linktr.ee/SunriseMvmtRVA" target="_blank">Sunrise Movement RVA</a></li>
+        <li><a href="https://www.instagram.com/richmond_wds" target="_blank">Richmond Weelaunee Defense Society</a></li>
     </ul>
     <h2>Anti-War</h2>
     <ul>
-        <li>
-            <a href="https://linktr.ee/amp_rva" target="_blank">American Muslims for Palestine RVA</a>
-        </li>
-        <li>
-            <a href="https://www.instagram.com/sjprva" target="_blank">Students for Justice in Palestine in RVA</a>
-        </li>
-        <li>
-            <a href="https://linktr.ee/richmondFNB" target="_blank">Richmond Food Not Bombs</a>
-        </li>
+        <li><a href="https://linktr.ee/amp_rva" target="_blank">American Muslims for Palestine RVA</a></li>
+        <li><a href="https://www.instagram.com/sjprva" target="_blank">Students for Justice in Palestine in RVA</a></li>
+        <li><a href="https://linktr.ee/richmondFNB" target="_blank">Richmond Food Not Bombs</a></li>
     </ul>
     <h2>Anti-Oppression</h2>
     <ul>
-        <li>
-            <a href="https://msha.ke/songrva" target="_blank">Southerners on New Ground RVA</a>
-        </li>
-        <li>
-            <a href="https://www.instagram.com/richmond_wds" target="_blank">Richmond Weelaunee Defense Society</a>
-        </li>
-        <li>
-            <a href="https://www.rrfp.net" target="_blank">Richmond Reproductive Freedom Project</a>
-        </li>
+        <li><a href="https://msha.ke/songrva" target="_blank">Southerners on New Ground RVA</a></li>
+        <li><a href="https://www.instagram.com/richmond_wds" target="_blank">Richmond Weelaunee Defense Society</a></li>
+        <li><a href="https://www.rrfp.net" target="_blank">Richmond Reproductive Freedom Project</a></li>
     </ul>
     <h2>Community Building</h2>
     <ul>
-        <li>
-            <a href="https://linktr.ee/rvaqueerbookclub">RVA Queer Book Club</a>
+        <li><a href="https://linktr.ee/rvaqueerbookclub" target="_blank">RVA Queer Book Club</a></li>
+        <li><a href="https://linktr.ee/dhrva" target="_blank">Dyke Hang RVA</a></li>
+        <li><a href="https://www.instagram.com/fbc.rva" target="_blank">Feminist Bird Club (birdwatching)</a></li>
+        <li><a href="https://linktr.ee/slamrva" target="_blank">Slam Richmond (poetry)</a></li>
+        <li><a href="https://www.instagram.com/bigqueercraftcircle" target="_blank">Big Queer Craft Circle (on hiatus)</a>
         </li>
-        <li><a href="https://linktr.ee/dhrva">Dyke Hang RVA</a></li>
-        <li>
-            <a href="https://www.instagram.com/fbc.rva">Feminist Bird Club (birdwatching)</a>
-        </li>
-        <li><a href="https://linktr.ee/slamrva">Slam Richmond (poetry)</a></li>
-        <li>
-            <a href="https://www.instagram.com/bigqueercraftcircle">Big Queer Craft Circle (on hiatus)</a>
-        </li>
-        <li>
-            <a href="https://www.instagram.com/flan.rva">FusterLAN (computer games)</a>
-        </li>
-        <li><a href="https://linktr.ee/rvaqueersoccer">RVA Queer Soccer</a></li>
+        <li><a href="https://www.instagram.com/flan.rva" target="_blank">FusterLAN (computer games)</a></li>
+        <li><a href="https://linktr.ee/rvaqueersoccer" target="_blank">RVA Queer Soccer</a></li>
+        <li><a href="https://www.instagram.com/rvaqueerpickleball" target="_blank">RVA Queer Pickleball</a></li>
     </ul>
     <h2>Events and organizations with reported transphobic elements</h2>
     <ul>
         <li>Roll Call RVA (Transphobic founder)</li>
-        <li>
-            RVA Smash/Smash the Byrd (Multiple reports of unsafe conditions for
-            trans people. Transphobic regular attendee.)
+        <li>RVA Smash/Smash the Byrd (Multiple reports of unsafe conditions for trans people. Transphobic regular attendee.)
         </li>
     </ul>
 </template>
