cd6b16b0893e62258c401416bb4c74a15af5f633
NatVIII <natviii@users.noreply.github.com>
2024-02-04 19:19:03 -0500

    Updated list.vue to be in line with Dannys changes

--- pages/list.vue
+++ pages/list.vue
@@ -4,10 +4,24 @@ import 'assets/style.css';
 
 <template>
     <div class="desc" style="line-height: 2;">
-        <h1>Richmond Community Events & Organizations</h1>
-        <h2>Events Aggregator</h2>
+        <h1>Queer-Friendly Community Organizations</h1>
+        <h2>Events Aggregators</h2>
         <ul>
             <li><a href="https://linktr.ee/commonalityrva" target="_blank">Commonality RVA</a></li>
+        </ul>
+        <h2>Community Building</h2>
+        <ul>
+            <li><a href="https://linktr.ee/rvaqueerbookclub" target="_blank">RVA Queer Book Club</a></li>
+            <li><a href="https://linktr.ee/dhrva" target="_blank">Dyke Hang RVA</a></li>
+            <li><a href="https://www.instagram.com/fbc.rva" target="_blank">Feminist Bird Club (birdwatching)</a></li>
+            <li><a href="https://linktr.ee/slamrva" target="_blank">Slam Richmond (poetry)</a></li>
+            <li><a href="https://www.instagram.com/bigqueercraftcircle" target="_blank">Big Queer Craft Circle (on hiatus)</a>
+            </li>
+            <li><a href="https://www.instagram.com/flan.rva" target="_blank">FusterLAN (computer games)</a></li>
+            <li><a href="https://linktr.ee/rvaqueersoccer" target="_blank">RVA Queer Soccer</a></li>
+            <li><a href="https://www.instagram.com/rvaqueerpickleball" target="_blank">RVA Queer Pickleball</a></li>
+            <li><a href="https://www.instagram.com/rvasquaredance" target="_blank">Richmond Square Dance (not queer, but
+                    queer-friendly)</a></li>
         </ul>
         <h2>Labor Rights</h2>
         <ul>
@@ -38,23 +52,12 @@ import 'assets/style.css';
             <li><a href="https://www.instagram.com/richmond_wds" target="_blank">Richmond Weelaunee Defense Society</a></li>
             <li><a href="https://www.rrfp.net" target="_blank">Richmond Reproductive Freedom Project</a></li>
         </ul>
-        <h2>Community Building</h2>
-        <ul>
-            <li><a href="https://linktr.ee/rvaqueerbookclub" target="_blank">RVA Queer Book Club</a></li>
-            <li><a href="https://linktr.ee/dhrva" target="_blank">Dyke Hang RVA</a></li>
-            <li><a href="https://www.instagram.com/fbc.rva" target="_blank">Feminist Bird Club (birdwatching)</a></li>
-            <li><a href="https://linktr.ee/slamrva" target="_blank">Slam Richmond (poetry)</a></li>
-            <li><a href="https://www.instagram.com/bigqueercraftcircle" target="_blank">Big Queer Craft Circle (on hiatus)</a>
-            </li>
-            <li><a href="https://www.instagram.com/flan.rva" target="_blank">FusterLAN (computer games)</a></li>
-            <li><a href="https://linktr.ee/rvaqueersoccer" target="_blank">RVA Queer Soccer</a></li>
-            <li><a href="https://www.instagram.com/rvaqueerpickleball" target="_blank">RVA Queer Pickleball</a></li>
-        </ul>
         <h2>Events and organizations with reported transphobic elements</h2>
         <ul>
             <li>Roll Call RVA (Transphobic founder)</li>
             <li>RVA Smash/Smash the Byrd (Multiple reports of unsafe conditions for trans people. Transphobic regular attendee.)
             </li>
+            <li>Glover Park pickup soccer (One report of unsafe conditions for trans people)</li>
         </ul>
     </div>
 </template>
