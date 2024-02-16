f07c5ad44aeec1690ec9def2cf69d0acf670a2d6
NatVIII <natviii@users.noreply.github.com>
2024-02-16 10:55:39 -0500

    init to Calendar Tutorial Page

---
+++ pages/contributing.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import 'assets/style.css';
+</script>
+
+<template>
+    <div class="desc" style="line-height: 2;">
+        <h1>How to Contribute your Calendar to rva.rip</h1>
+    </div>
+    <div>
+        <div style="background-color: #fb4934;" class="color-stripe"></div>
+        <div style="background-color: #fe8019;" class="color-stripe"></div>
+        <div style="background-color: #fabd2f;" class="color-stripe"></div>
+        <div style="background-color: #b8bb26;" class="color-stripe"></div>
+        <div style="background-color: #83a598;" class="color-stripe"></div>
+        <div style="background-color: #d3869b;" class="color-stripe"></div>
+        <div class="color-stripe"></div><div class="color-stripe"></div>
+    </div>
+</template>
