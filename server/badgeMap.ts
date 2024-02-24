5bf41ceec07b4b540dd685e24920925216cfe37f
NatVIII <natviii@users.noreply.github.com>
2024-02-24 12:27:33 -0500

    Added Outpost calendar
    Added badges with and without shadow to emphasize them

--- server/badgeMap.ts
+++ server/badgeMap.ts
@@ -1,5 +1,5 @@
 export const badgeMap: { [key: string]: string } = {
-    ':test:': '<img src="/badges/test.png" class="badge">', //By SPwins on Deviantart, CC Attribution 3.0 license, gotten from https://www.deviantart.com/spwins/art/Bmo-512x512-883561125
+    ':test:': '<img src="/badges/test.png" class="badge-shadow">', //By SPwins on Deviantart, CC Attribution 3.0 license, gotten from https://www.deviantart.com/spwins/art/Bmo-512x512-883561125
     ':coalition_theater:': '<img src="/badges/coalition_theater.svg" class="badge">',
     ':fojrp:': '<img src="/badges/fojrp.png" class="badge">',
     ':dykes_on_bikes:': '<img src="/badges/dykes_on_bikes.png" class="badge">',
@@ -7,7 +7,8 @@ export const badgeMap: { [key: string]: string } = {
     ':twitter:': '<img src="/badges/twitter.svg" class="badge">',
     ':hackrva:': '<img src="/badges/hackrva.svg" class="badge">',
     ':madrva:': '<img src="/badges/madrva.png" class="badge">',
-    ':sunrise:': '<img src="/badges/sunrise.svg" class="badge">',
-    ':firstfridays:': '<img src="/badges/firstfridays.png" class="badge">',
-    ':kasamacollective:': '<img src="/badges/kasamacollective.png" class="badge">',
+    ':sunrise:': '<img src="/badges/sunrise.svg" class="badge-shadow">',
+    ':firstfridays:': '<img src="/badges/firstfridays.png" class="badge-shadow">',
+    ':kasamacollective:': '<img src="/badges/kasamacollective.png" class="badge-shadow">',
+    ':outpost:': '<img src="/badges/outpost.svg" class="badge-shadow">',
   };
