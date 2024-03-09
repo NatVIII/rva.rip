1f6f5503d57589b81b5a9d787ade805860ee57a2
NatVIII <natviii@users.noreply.github.com>
2024-03-08 22:54:48 -0500

    Fixed Mode Selection UI from not showing proper preference on page load

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 import { useTheme } from '@/composables/useTheme';
+import { watch, onMounted } from 'vue';
 
 const { theme, setTheme } = useTheme();
 
 const switchTheme = (newTheme) => setTheme(newTheme);
+
+// This could potentially force an update if necessary, but might not be needed
+// if your initial setup already correctly handles this.
+onMounted(() => {
+  const currentTheme = theme.value;
+  switchTheme('default');
+  setTimeout(() => {
+    switchTheme(currentTheme); // Re-apply the current theme to ensure UI consistency
+  }, 0); // A delay of 0 ms still allows the event loop to process pending tasks
+});
+
+// Watch for changes in the theme and log the new value to the console
+watch(theme, (newTheme, oldTheme) => {
+  console.log(`Theme changed from ${oldTheme} to ${newTheme}`);
+}, { immediate: true });
 </script>
 
 <template>
@@ -12,11 +28,13 @@ const switchTheme = (newTheme) => setTheme(newTheme);
         <a href="/">home</a> |
         <a href="/list">list of cool groups</a> |
         <a href="/contributing">how to contribute</a> |
-        <a>color theme: </a>
-        <span :style="{ filter: theme === 'dark' ? 'drop-shadow(0px 0px 0.25em var(--border-outer))' : '' }" @click="switchTheme('dark')" class="link-like">🌑</span> 
-        <span :style="{ filter: theme === 'default' ? 'drop-shadow(0px 0px 0.25em var(--border-outer))' : '' }" @click="switchTheme('default')" class="link-like">🌓</span> 
-        <span :style="{ filter: theme === 'light' ? 'drop-shadow(0px 0px 0.25em var(--border-outer))' : '' }" @click="switchTheme('light')" class="link-like">🌕</span> |
-        <a href="https://github.com/natviii/rva.rip/">source code</a> 
+        <span class="window-box-effect"> color theme:  
+          <span class="window-box-inner">
+          <span @click="switchTheme('dark')" class="link-like" :class="{'selected-preference': theme === 'dark'}">🌑</span> <a> </a>
+          <span @click="switchTheme('default')" class="link-like" :class="{'selected-preference': theme === 'default'}">🌓</span> <a> </a>
+          <span @click="switchTheme('light')" class="link-like" :class="{'selected-preference': theme === 'light'}">🌕</span>
+        </span> </span>
+        | <a href="https://github.com/natviii/rva.rip/">source code</a> 
       </div>
       <div style="background-color: var(--rip-red);" class="color-stripe"></div>
       <div style="background-color: var(--rip-orange);" class="color-stripe"></div>
