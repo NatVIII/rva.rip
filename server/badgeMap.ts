01576e1d10073aedde4c5e221cd04793ac51ff11
NatVIII <natviii@users.noreply.github.com>
2024-03-10 00:20:49 -0500

    Added Studio Two Three Emote

--- server/badgeMap.ts
+++ server/badgeMap.ts
@@ -3,10 +3,10 @@ export const badgeMap: { [key: string]: string } = {
     ':coalition_theater:': '<img src="/badges/coalition_theater.svg" class="badge">',
     ':fojrp:': '<img src="/badges/fojrp.png" class="badge">',
     ':dykes_on_bikes:': '<img src="/badges/dykes_on_bikes.png" class="badge-shadow-w">',
-    ':ripple:': '<img src="/badges/ripple.svg" class="badge-shadow-w">',
+    ':ripple:': '<img src="/badges/ripple.svg" class="badge invert-dark">',
     ':twitter:': '<img src="/badges/twitter.svg" class="badge">',
     ':hackrva:': '<img src="/badges/hackrva.svg" class="badge">',
-    ':madrva:': '<img src="/badges/madrva.svg" class="badge-shadow-w">',
+    ':madrva:': '<img src="/badges/madrva.svg" class="badge invert-dark">',
     ':sunrise:': '<img src="/badges/sunrise.svg" class="badge-shadow-b">',
     ':firstfridays:': '<img src="/badges/firstfridays.png" class="badge-shadow-b">',
     ':kasamacollective:': '<img src="/badges/kasamacollective.svg" class="badge-shadow-b">',
@@ -15,4 +15,5 @@ export const badgeMap: { [key: string]: string } = {
     ':goldlion:': '<img src="/badges/goldlion.svg" class="badge-shadow-b">',
     ':RVAGrowsGardens1:': '<img src="/badges/RVAGrowsGardens1.svg" class="badge-shadow-b">',
     ':RVAGrowsGardens2:': '<img src="/badges/RVAGrowsGardens2.svg" class="badge">',
+    ':studiotwothree:': '<img src="/badges/studiotwothree.svg" class="badge invert-dark">',
   };
