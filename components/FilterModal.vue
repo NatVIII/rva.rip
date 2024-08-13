2df6251bf3fd93122422c7a54d4e34203966f66d
NatVIII <natviii@users.noreply.github.com>
2024-08-13 08:36:03 -0400

    💄 Some more UI Work on FilterModal

--- components/FilterModal.vue
+++ components/FilterModal.vue
@@ -32,45 +32,26 @@ function handleEventSourceChange(tag: string, isEnabled: boolean) {
   <VueFinalModal class="popper-box-wrapper" content-class="popper-box-inner" overlay-transition="vfm-fade" content-transition="vfm-fade">
     <span class="event-headers">
       Event Purpose
-      <div v-for="tag in headerTags" :key="tag" class="event-details">
-        <input type="checkbox" :id="tag" @change="handleEventSourceChange(tag, $event.target.checked)" />
-        <label for="tag">{{ tag }}</label>
-      </div>
     </span>
+    <TagFilterItem v-for="tag in headerTags" :key="tag" class="tag-group" :label="tag">
+    </TagFilterItem>
     <span class="event-headers">
       Event Type
-      <div v-for="group in tagsToShow" :key="group[0] || group" class="tag-group">
-        <template v-if="Array.isArray(group)">
-          <div class="tag-header">
-            <input type="checkbox" :id="group[0]" @change="handleEventSourceChange(group[0], $event.target.checked)" />
-            <label for="group[0]">{{ group[0] }}</label>
-          </div>
-          <div v-for="tag in group.slice(1)" :key="tag" class="tag-sub-item">
-            <input type="checkbox" :id="tag" @change="handleEventSourceChange(tag, $event.target.checked)" />
-            <label for="tag">{{ tag }}</label>
-          </div>
-        </template>
-        <template v-else>
-          <div class="tag-group-single">
-            <input type="checkbox" :id="group" @change="handleEventSourceChange(group, $event.target.checked)" />
-            <label for="group">{{ group }}</label>
-          </div>
-        </template>
-      </div>
     </span>
+    <div v-for="group in tagsToShow" :key="group[0] || group" class="tag-group">
+      <template v-if="Array.isArray(group)">
+        <TopicFilterItem class="tag-header" :label="group[0]">
+          <TagFilterItem v-for="tag in group.slice(1)" :key="tag" class="tag-sub-item" :label="tag">
+          </TagFilterItem>
+        </TopicFilterItem>
+      </template>
+      <template v-else>
+        <TagFilterItem class="tag-sub-item" :label="group">
+        </TagFilterItem>
+      </template>
+    </div>
     <div class="bottom">
       <button @click="emit('confirm')">Apply</button>
     </div>
   </VueFinalModal>
 </template>
-
-<style scoped>
-.tag-header {
-  font-weight: bold;
-  margin-top: 1rem;
-}
-
-.tag-sub-item {
-  margin-left: 20px;
-}
-</style>
