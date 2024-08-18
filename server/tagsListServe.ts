5dbf15bfdf981cbeeef7b9a4695f47d3e97442f7
NatVIII <natviii@users.noreply.github.com>
2024-08-18 11:20:04 -0400

    🗃️💡 Added isHidden and isHeader to tags array w/ comments

--- server/tagsListServe.ts
+++ server/tagsListServe.ts
@@ -3,6 +3,8 @@ import eventSourcesJSON from '@/assets/event_sources.json';
 export interface Tag {
   name: string;
   isVisible: boolean;
+  isHidden: boolean;
+  isHeader: boolean;
 }
 
 // Helper function to extract source names from URLs
@@ -45,10 +47,13 @@ export function getAllTags(): Tag[] {
   });
 
   const tagsHidden = new Set(eventSourcesJSON.appConfig.tagsHidden);
+  const tagsHeader = new Set(eventSourcesJSON.appConfig.tagsHeader);
 
   // Convert the set of tags into an array of Tag objects, setting visibility based on tagsHidden
   return Array.from(tagsSet).map(tag => ({
     name: tag,
-    isVisible: !tagsHidden.has(tag)
+    isVisible: !tagsHidden.has(tag),  //Whether a tag is visible, this is meant to be updated on the user side
+    isHidden: tagsHidden.has(tag),    //Whether a tag indicates that it ought to be hidden. This is permanent, if a tag has isHidden true then any event with it ought to be hidden forever
+    isHeader: tagsHeader.has(tag)     //Whether a tag is a header tag, this means that it's a pre-requisite that atleast one visible Header tag should be on an event for the event to be visible at all.
   }));
 }
