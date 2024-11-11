1bb99bc02885b94a73fde56ed536b347e77ad2eb
Devin M. Ceartas <devin@nacredata.com>
2024-11-11 13:04:31 -0500

    grabed new link tree for the list page

--- pages/list.vue
+++ pages/list.vue
@@ -3,32 +3,9 @@
 
 <template>
     <div class="desc" style="line-height: 2;">
-        <h1>Queer-Friendly Community Organizations</h1>
-        <h2>Events Aggregators</h2>
-        <ul>
-            
-        </ul>
-        <h2>Community Building</h2>
-        <ul>
-        </ul>
-        <h2>Labor Rights</h2>
-        <ul>
-        </ul>
-        <h2>Mutual Aid</h2>
-        <ul>
-        </ul>
-        <h2>Climate Justice</h2>
-        <ul>
-        </ul>
-        <h2>Anti-War</h2>
-        <ul>
-        </ul>
-        <h2>Anti-Oppression</h2>
-        <ul>
-        </ul>
-        <h2>Events and organizations with reported transphobic elements</h2>
-        <ul>
-        </ul>
+        <h1>Community Organizations</h1>
+        <p><a href="https://linktr.ee/RhizomeResources">Please see these links for 
+        organizations to plug into in the NC Triangle</a></p>
     </div>
     <Footer />
 </template>
